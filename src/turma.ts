1287a80f9f4af819d7944c6b0f36e10636f1a3bf
Fagner Fontes Batista <fagnerfontesbatista@hotmail.com>
2023-01-03 21:11:08 -0300

    Criação de classes

---
+++ src/turma.ts
@@ -0,0 +1,23 @@
+import { Pessoa } from "./pessoa";
+
+export class tuwrma{
+    id: string; 
+    nomeTurma: string; 
+    docentes: string[]; 
+    estudantes: string[]; 
+    modulo: string;  
+constructor(
+    id:string, 
+    nomeTuma:string, 
+    email:string, 
+    dataNascimento: Date, 
+    turma: string, 
+){
+    this.id,
+    this.nomeTurma, 
+    this.docentes, 
+    this.estudantes, 
+    this.modulo
+}
+} 
+
